2cd9546f73245aad614c9cbb91e58e925981d4bc
samuel <monkeygmaming222@gmail.com>
2025-09-04 22:20:16 -0400

    firstCommitOfMyGame

--- Assets/script/playerMovement.cs
+++ Assets/script/playerMovement.cs
@@ -6,13 +6,17 @@ using System.Runtime.Serialization.Formatters;
 using Unity.VisualScripting;
 using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.Animations;
 using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [SerializeField] AudioClip sfxJump;
+    [SerializeField] AudioClip sfxWalk;
+    private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
     private float xAxis;
+    private float yaxis;
     [SerializeField] private float walkspeed = 6f;
 
     [SerializeField] private float jumpForce = 10f;
@@ -21,12 +25,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int jumMax = 1;
 
     private int currentJump = 0;
+    private AudioSource audioSource;
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 2f;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
+        
+
     }
 
     // Update is called once per frame
@@ -44,6 +55,7 @@ public class PlayerMovement : MonoBehaviour
 
             if (currentJump < 2)
             {
+                audioSource.PlayOneShot(sfxJump);
                 rb.velocity = new Vector2(rb.velocity.x, 10f);
                 Debug.Log(currentJump + " " + isgrounded);
                 currentJump += 1; 
@@ -56,14 +68,31 @@ public class PlayerMovement : MonoBehaviour
     void GetInputs()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
-
+        if (xAxis > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (xAxis < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+       
 
     }
     private void Move()
     {
 
         rb.velocity = new Vector2(walkspeed * xAxis, rb.velocity.y);
-
+        if (xAxis != 0 && isgrounded && !audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(sfxWalk);
+        }
+        yaxis = Input.GetAxisRaw("Vertical");
+        Debug.Log(yaxis);
+        if (xAxis == 0 && yaxis! > 0)
+        {
+            audioSource.Stop();
+        }
     }
 
     public void setJumMax(int value)
